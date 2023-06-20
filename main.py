2ff4440d52e8b8c8c87e941b38f1083369351d40
Aleksander <spas.aleksander.v@gmail.com>
2023-06-20 14:32:48 +0300

    Все готово

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ print(n)
 n = 'sir'
 print(n)
 
-n1 = "Sir Roland"
+n1 = "Sir\" Roland"
 
 print(type(n))
+print(n1)
+
+
+print('Введите значение: ')
+a = input()
+print(a)
+
+r = range(23)
+print(type(r))
